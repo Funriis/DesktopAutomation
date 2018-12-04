a7a865da3ff355cb28d6c7a2424454e036c51407
Funris <mouhibbennasser11@gmail.com>
2018-12-04 09:51:55 +0100

    First commit

---
+++ clickBtn.py
@@ -0,0 +1,5 @@
+import subprocess
+import tkinter as tk
+
+def clicked():
+  print("A small change to this file for testing")
---
+++ window.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from clickBtn import clicked
+
+#Todo: Learn more about tkinter grids
+#Todo: Think of something you can automate with a window/buttons
+#*imp: Don't let that younger kid get better (Texas tho)
+#*imp: Udemy course on data visulization
+
+#Git init                         (Initialize git)
+#rm -rf .git                      (Remove git)
+#Git add .                        (Add all files)
+#Git commit -m "Commit desc"      (Commit)
+
+window = tk.Tk()
+window.geometry("800x500+200+200")
+
+Button_1 = tk.Button(window, text="Open Google chrome", bg="#545454", command=clicked)
+Button_1.pack()
+
+window.mainloop()
